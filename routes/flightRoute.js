40f52fd7025258c48f349f752122b193b3b52f0a
StokeBalance <81008223+StokeBalance@users.noreply.github.com>
2022-11-05 10:35:19 +0100

    Update flightRoute.js

--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -3,7 +3,14 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/flightController');
 
-router.get('/', controller.example)
+router
+    .get('/', controller.getAllFlights)
+    .post('/', controller.createFlight)
+    .get('/:id', controller.getFlight)
+    .put('/:id', controller.updateFlight)
+    .delete('/:id', controller.deleteFlight)
 
 module.exports = router;
+
+
 
